59e3de0f30d35267bc34da363c79d54b1a25a524
idd_pjesus <idd_pjesus@uolinc.com>
2024-12-09 21:02:24 -0300

    start project

---
+++ src/main/java/com/pedroajs/mysongapi/MysongapiApplication.java
@@ -0,0 +1,13 @@
+package com.pedroajs.mysongapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysongapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysongapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroajs/mysongapi/MysongapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroajs.mysongapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysongapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
